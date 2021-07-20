ce9da1cc0ec1a51b0675aa8ce1e7c5b31f076cfe
Ethan Nguyen <ethan.nguyen@onfido.com>
2021-07-20 13:27:11 +0100

    Add prettier

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet } from "react-native";
-import WebView from "react-native-webview";
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import WebView from 'react-native-webview'
 
 export default function App() {
   return (
     <React.Fragment>
       <WebView
-        source={{ uri: "https://latest-onfido-sdk-ui-onfido.surge.sh/" }}
+        source={{ uri: 'https://latest-onfido-sdk-ui-onfido.surge.sh/' }}
         style={styles.webView}
       />
       <StatusBar style="auto" />
     </React.Fragment>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -21,8 +21,8 @@ const styles = StyleSheet.create({
     marginBottom: 50,
     marginLeft: 10,
     marginRight: 10,
-    backgroundColor: "transparent",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
